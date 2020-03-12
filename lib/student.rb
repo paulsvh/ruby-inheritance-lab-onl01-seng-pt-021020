418ba5816a8c03d9b03035ab76dbf840f4ce4d0f
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-12 00:49:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,17 @@ require_relative "./user.rb"
 
 class Student < User
 
+  def initialize
+    knowledge = []
+  end
 
+  def learn 
+    teacher.teach << knowledge
+  end
+
+  def knowledge
+    knowledge
+  end
 
 
 end
